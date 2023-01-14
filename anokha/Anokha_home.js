8357145da7b78c8e22672594eb12c2872992cdf0
Santhossh K S <92919726+santhossh5@users.noreply.github.com>
2023-01-14 21:07:40 +0530

    Update Anokha_home.js

--- anokha/Anokha_home.js
+++ anokha/Anokha_home.js
@@ -1,13 +1,13 @@
-let mybutton = document.getElementById("myBtn");
+let myButton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-    mybutton.style.display = "block";
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 
